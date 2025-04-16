67c008acd321098497af966111894902df9fb468
Ad-Matey <Ad.Matey11@Gmail.com>
2025-04-16 13:26:19 -0500

    Updated Flask app to use CORS

--- code/app.py
+++ code/app.py
@@ -1,9 +1,11 @@
 
 from flask import Flask
+from flask_cors import CORS
 import json
 from inference import getPredictions
 
 app = Flask(__name__)
+CORS(app)
 
 # Endpoint to get predictions of all the models when passed text as an input
 # Returns: JSON object consisting of pairs in the format:
